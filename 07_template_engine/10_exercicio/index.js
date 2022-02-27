580a9e1aa68328945c34972d2a59e4945b1f1384
Fabio Cerqueira <fabiocerq.esa@hotmail.com>
2022-02-27 15:45:03 -0300

    Secao 7 - Parte 2

--- 07_template_engine/10_exercicio/index.js
+++ 07_template_engine/10_exercicio/index.js
@@ -1,17 +1,72 @@
 const express = require('express');
-const app = express();
-const port = 3000;
 const exphbs = require('express-handlebars');
 
+const app = express();
 
 app.engine('handlebars', exphbs.engine())
-
 app.set('view engine','handlebars')
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.use(express.static('public'))
+
+/* app.get('/camisa', (req,res) => {
+    res.render('camisa')
 })
 
-app.listen(port, () => {
-    console.log(`App rodando na porta ${port}`)
+app.get('/calca', (req,res) => {
+    res.render('calca')
+})
+
+app.get('/bone', (req,res) => {
+    res.render('bone')
+}) */
+
+const products = [
+    {
+        id: '1',
+        name: "Camisa T-Shirt",
+        price: 48.99
+    },
+    {
+        id: '2',
+        name: "Boné Aba Reta",
+        price: 55.00,
+    },
+    {
+        id: '3',
+        name: "Calça Slim",
+        price: 89.99
+    }
+]
+
+app.get('/', (req, res) => {
+
+/*     const product = [
+        {
+            ID: 'camisa',
+            name: "Camisa T-Shirt",
+            price: 48.99
+        },
+        {
+            ID: 'bone',
+            name: "Boné Aba Reta",
+            price: 55.00,
+        },
+        {
+            ID: 'calca',
+            name: "Calça Slim",
+            price: 89.99
+        }
+    ]
+ */
+    res.render('home', {products})
+})
+
+app.get('/product/:id', (req, res) => {
+    const product = products[parseInt(req.params.id)]
+    
+    res.render('product', {product})
+})
+
+app.listen(3000, () => {
+    console.log(`App na porta 3000`)
 })
