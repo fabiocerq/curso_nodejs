1171e52eb5413471bb1cdcf9ce54edf0d5472f78
Fabio Cerqueira <fabiocerq.esa@hotmail.com>
2022-04-22 21:37:15 -0300

    Secao 11 - Parte 4

--- 11_2_projeto_toughts/controllers/ToughtController.js
+++ 11_2_projeto_toughts/controllers/ToughtController.js
@@ -7,10 +7,101 @@ module.exports = class ToughtsController {
     }
 
     static async dashboard(req, res) {
-        res.render('toughts/dashboard')
+        const userId = req.session.userid
+
+        const user = await User.findOne({
+            where: {
+                id: userId
+            },
+            include: Tought, //puxa cada pensamento diretamente sem precisar de uma query
+            plain: true //tratamento de dados
+        })
+
+        //check user existence
+        if (!user) {
+            res.redirect('/login')
+        }
+
+        const toughts = user.Toughts.map((result) => result.dataValues) //isolando parte que interessa do dado
+
+        let emptyToughts = false
+
+        if (toughts.length === 0) {
+            emptyToughts = true
+        }
+
+        res.render('toughts/dashboard', { toughts, emptyToughts })
     }
 
     static createTought(req, res) {
         res.render('toughts/create')
+    }
+
+    static async createToughtSave(req, res) {
+
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid
+        }
+
+        try {
+            await Tought.create(tought)
+
+            req.flash('message', 'Pensamento criado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+
+        } catch (error) {
+            console.log('Erro: ' + error)
+        }
+    }
+
+    static async removeTought(req, res) {
+
+        const id = req.body.id
+        const UserId = req.session.userid
+
+        try {
+            await Tought.destroy({ where: { id: id, UserId: UserId } })
+
+            req.flash('message', 'Pensamento removido com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log('Erro: ' + error)
+        }
+
+    }
+
+    static async updateTought(req, res) {
+        const id = req.params.id
+
+        const tought = await Tought.findOne({ where: { id: id }, raw: true })
+
+        res.render('toughts/edit', { tought })
+    }
+
+    static async updateToughtSave(req, res) {
+        const id = req.body.id
+
+        const tought = {
+            title: req.body.title,
+        }
+
+        try {
+            await Tought.update(tought, { where: { id: id } })
+
+            req.flash('message', 'Pensamento atualizado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log('Erro: ' + error)
+        }
     }
 }
